bd89a85c90168e959df65d34f7f5a37529f3b646
vlad <arefin.vlad@gmail.com>
2025-04-03 15:53:10 +0300

    #4 Feat: create Repositories contracts skeleton

---
+++ TweetService.Application/Contracts/RepositoryContracts/IRepositoryBase.cs
@@ -0,0 +1,8 @@
+﻿namespace TweetService.Application.Contracts.RepositoryContracts;
+
+public interface IRepositoryBase<T>
+{
+    Task CreateAsync(T entity, CancellationToken cancellationToken);
+    void Delete(T entity);
+    void Update(T entity);
+}
