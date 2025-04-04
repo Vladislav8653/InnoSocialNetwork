c9f9e815588695a51bdc26c4338c6b882b09dedb
vlad <arefin.vlad@gmail.com>
2025-04-04 18:13:08 +0300

    #6 Feat: create queries usecases for tweets

---
+++ TweetService.Application/UseCases/Queries/Tweet/GetTweetById/GetTweetByIdCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using TweetService.Application.DTOs.TweetsDto;
+
+namespace TweetService.Application.UseCases.Queries.Tweet.GetTweetById;
+
+public class GetTweetByIdCommandHandler : IRequestHandler<GetTweetByIdCommand, TweetResponseToDto>
+{
+    public async Task<TweetResponseToDto> Handle(GetTweetByIdCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
