4ed37d99343ffd4427c466c9c5178b7c37aad503
vlad <arefin.vlad@gmail.com>
2025-04-07 23:17:42 +0300

    #18 Feat: create endpoints with CRUD operations

---
+++ TweetService.Application/UseCases/Commands/Tweet/DeleteTweet/DeleteTweetCommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace TweetService.Application.UseCases.Commands.Tweet.DeleteTweet;
+
+public class DeleteTweetCommandHandler : IRequestHandler<DeleteTweetCommand, Unit>
+{
+    public async Task<Unit> Handle(DeleteTweetCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
--- TweetService.Application/UseCases/Queries/Tweet/GetTweets/GetTweetsCommand.cs
+++ TweetService.Application/UseCases/Queries/Tweet/GetTweets/GetTweetsCommand.cs
@@ -1,6 +1,10 @@
 ﻿using MediatR;
 using TweetService.Application.DTOs.TweetsDto;
+using TweetService.Application.Pagination;
 
 namespace TweetService.Application.UseCases.Queries.Tweet.GetTweets;
 
-public record GetTweetsCommand : IRequest<IEnumerable<TweetResponseToDto>>;
+public record GetTweetsCommand : IRequest<IEnumerable<TweetResponseToDto>>
+{
+    public PageParams PageParams { get; init; } = null!;
+}
