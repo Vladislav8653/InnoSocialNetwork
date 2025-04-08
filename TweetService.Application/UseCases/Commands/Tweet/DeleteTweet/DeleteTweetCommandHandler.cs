6d1173c91d4c8c7c130e603892b0d7bd87377758
vlad <arefin.vlad@gmail.com>
2025-04-08 09:44:38 +0300

    #18 Feat: delete tweet use case

--- TweetService.Application/UseCases/Commands/Tweet/DeleteTweet/DeleteTweetCommandHandler.cs
+++ TweetService.Application/UseCases/Commands/Tweet/DeleteTweet/DeleteTweetCommandHandler.cs
@@ -1,11 +1,34 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
+using TweetService.Application.Contracts.RepositoryContracts;
 
 namespace TweetService.Application.UseCases.Commands.Tweet.DeleteTweet;
 
-public class DeleteTweetCommandHandler : IRequestHandler<DeleteTweetCommand, Unit>
+public class DeleteTweetCommandHandler(
+    ITweetRepository tweetRepository) :
+    IRequestHandler<DeleteTweetCommand, Unit>
 {
     public async Task<Unit> Handle(DeleteTweetCommand request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        if (!Guid.TryParse(request.UserId, out var userIdGuid))
+        {
+            throw new ValidationException("UserId is invalid");
+        }
+        
+        var tweets = await tweetRepository
+            .FindByConditionAsync(tweet => tweet.Id == request.TweetId,
+                false, cancellationToken);
+        var tweet = tweets.FirstOrDefault();
+        if (tweet is null)
+            throw new InvalidOperationException($"Tweet with id {request.TweetId} not found");
+        
+        if (tweet.WriterId != userIdGuid)
+        {
+            throw new UnauthorizedAccessException("User is not authorized to delete this product");
+        }
+        
+        await tweetRepository.DeleteAsync(tweet, cancellationToken);
+        
+        return Unit.Value;
     }
 }
