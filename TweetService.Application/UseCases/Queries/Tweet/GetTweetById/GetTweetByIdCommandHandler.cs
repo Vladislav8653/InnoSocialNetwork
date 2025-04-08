6ca9669575fdf112ed9ae650af5423431d9754f6
vlad <arefin.vlad@gmail.com>
2025-04-08 09:05:07 +0300

    #18 Feat: get tweet by id

--- TweetService.Application/UseCases/Queries/Tweet/GetTweetById/GetTweetByIdCommandHandler.cs
+++ TweetService.Application/UseCases/Queries/Tweet/GetTweetById/GetTweetByIdCommandHandler.cs
@@ -1,12 +1,25 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
+using TweetService.Application.Contracts.RepositoryContracts;
 using TweetService.Application.DTOs.TweetsDto;
 
 namespace TweetService.Application.UseCases.Queries.Tweet.GetTweetById;
 
-public class GetTweetByIdCommandHandler : IRequestHandler<GetTweetByIdCommand, TweetResponseToDto>
+public class GetTweetByIdCommandHandler(
+    ITweetRepository tweetRepository,
+    IMapper mapper) :
+    IRequestHandler<GetTweetByIdCommand, TweetResponseToDto>
 {
     public async Task<TweetResponseToDto> Handle(GetTweetByIdCommand request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var products = await tweetRepository.FindByConditionAsync(
+            product => product.Id == request.Id,false, cancellationToken);
+        var product = products.FirstOrDefault();
+        if (product is null)
+            throw new InvalidOperationException($"Tweet with id {request.Id} not found");
+        
+        var productResponseDto = mapper.Map<TweetResponseToDto>(product);
+        
+        return productResponseDto;
     }
 }
