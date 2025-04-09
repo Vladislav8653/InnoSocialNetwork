9a34db6ecc609bc2ac9d109def04ffb9bc9ce0a2
vlad <arefin.vlad@gmail.com>
2025-04-09 12:25:17 +0300

    #20 Feat: Get sticker by id use case

--- TweetService.Application/UseCases/Queries/Sticker/GetStickerById/GetStickerByIdCommandHandler.cs
+++ TweetService.Application/UseCases/Queries/Sticker/GetStickerById/GetStickerByIdCommandHandler.cs
@@ -1,12 +1,25 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
+using TweetService.Application.Contracts.RepositoryContracts;
 using TweetService.Application.DTOs.StickersDto;
 
 namespace TweetService.Application.UseCases.Queries.Sticker.GetStickerById;
 
-public class GetStickerByIdCommandHandler : IRequestHandler<GetStickerByIdCommand, StickerResponseDto>
+public class GetStickerByIdCommandHandler(
+    IStickerRepository stickerRepository,
+    IMapper mapper) : 
+    IRequestHandler<GetStickerByIdCommand, StickerResponseDto>
 {
     public async Task<StickerResponseDto> Handle(GetStickerByIdCommand request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var stickers = await stickerRepository.FindByConditionAsync(
+            sticker => sticker.Id == request.Id,false, cancellationToken);
+        var sticker = stickers.FirstOrDefault();
+        if (sticker is null)
+            throw new InvalidOperationException($"Sticker with id {request.Id} not found");
+        
+        var stickerResponseDto = mapper.Map<StickerResponseDto>(sticker);
+        
+        return stickerResponseDto;
     }
 }
