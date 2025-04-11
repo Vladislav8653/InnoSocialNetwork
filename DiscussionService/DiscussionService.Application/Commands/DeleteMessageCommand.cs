d1c8eeb973264d7333108bc076ace775fc8edd51
vlad <arefin.vlad@gmail.com>
2025-04-11 17:48:31 +0300

    #28 Feat: delete message use case

--- DiscussionService/DiscussionService.Application/Commands/DeleteMessageCommand.cs
+++ DiscussionService/DiscussionService.Application/Commands/DeleteMessageCommand.cs
@@ -1,8 +1,9 @@
-﻿using MongoDB.Bson;
+﻿using MediatR;
+using MongoDB.Bson;
 
 namespace DiscussionService.Application.Commands;
 
-public record DeleteMessageCommand
+public record DeleteMessageCommand : IRequest<Unit>
 {
     public ObjectId MessageId { get; init; }
 }
--- DiscussionService/DiscussionService.Application/UseCases/DeleteMessageCommandHandler.cs
+++ DiscussionService/DiscussionService.Application/UseCases/DeleteMessageCommandHandler.cs
@@ -1,6 +1,21 @@
-﻿namespace DiscussionService.Application.UseCases;
+﻿using DiscussionService.Application.Commands;
+using DiscussionService.Application.Contracts;
+using MediatR;
 
-public class DeleteMessageCommandHandler
+namespace DiscussionService.Application.UseCases;
+
+public class DeleteMessageCommandHandler (
+    IMessageRepository messageRepository) :
+    IRequestHandler<DeleteMessageCommand, Unit>
 {
-    
+    public async Task<Unit> Handle(DeleteMessageCommand request, CancellationToken cancellationToken)
+    {
+        var message = await messageRepository.GetByIdAsync(request.MessageId);
+        if (message is null)
+            throw new InvalidOperationException($"Message with id {request.MessageId} not found");
+        
+        await messageRepository.DeleteAsync(request.MessageId);
+        
+        return Unit.Value;
+    }
 }
