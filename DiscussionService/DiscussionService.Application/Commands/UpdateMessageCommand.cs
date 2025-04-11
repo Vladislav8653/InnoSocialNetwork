408327ef5f772adf9c635cce3bde3937f8c8e219
vlad <arefin.vlad@gmail.com>
2025-04-11 17:45:16 +0300

    #28 Feat: update message use case

--- DiscussionService/DiscussionService.Application/Commands/UpdateMessageCommand.cs
+++ DiscussionService/DiscussionService.Application/Commands/UpdateMessageCommand.cs
@@ -1,9 +1,10 @@
 ﻿using DiscussionService.Application.DTOs;
+using MediatR;
 using MongoDB.Bson;
 
 namespace DiscussionService.Application.Commands;
 
-public record UpdateMessageCommand
+public record UpdateMessageCommand : IRequest<Unit>
 {
     public ObjectId MessageId { get; init; }
     public MessageRequestDto MessageDto  { get; init; } 
---
+++ DiscussionService/DiscussionService.Application/UseCases/UpdateMessageCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using DiscussionService.Application.Commands;
+using DiscussionService.Application.Contracts;
+using DiscussionService.Domain.Models;
+using FluentValidation;
+using MediatR;
+
+namespace DiscussionService.Application.UseCases;
+
+public class UpdateMessageCommandHandler(
+    IMessageRepository messageRepository,
+    IMapper mapper,
+    IValidator<Message> validator) : 
+    IRequestHandler<UpdateMessageCommand, Unit>
+{
+    public async Task<Unit> Handle(UpdateMessageCommand request, CancellationToken cancellationToken)
+    {
+        var message = await messageRepository.GetByIdAsync(request.MessageId);
+        if (message is null)
+            throw new InvalidOperationException($"Message with id {request.MessageId} not found");
+            
+        mapper.Map(request.MessageDto, message);
+        
+        var validationResult = await validator.ValidateAsync(message, cancellationToken);
+        if (!validationResult.IsValid)
+            throw new ValidationException(validationResult.Errors);
+        
+        await messageRepository.UpdateAsync(message, request.MessageId);
+        
+        return Unit.Value;
+    }
+}
