08c2acbefb97530ae45bb6093fa012de327d78e8
vlad <arefin.vlad@gmail.com>
2025-04-11 11:08:23 +0300

    #26 Feat: create repository contracts

---
+++ DiscussionService/DiscussionService.Application/IMessageRepository.cs
@@ -0,0 +1,13 @@
+﻿using DiscussionService.Domain.Models;
+
+namespace DiscussionService.Application;
+
+public interface IMessageRepository
+{
+    Task<Message> GetByTweetIdAsync(Guid tweetId);
+    Task<IEnumerable<Message>> GetAllAsync();
+    Task CreateAsync(Message message);
+    Task DeleteAsync(Guid messageId);
+    Task UpdateAsync(Message message, Guid messageId);
+    
+}
---
+++ DiscussionService/DiscussionService.Domain/Models/Message.cs
@@ -0,0 +1,8 @@
+﻿namespace DiscussionService.Domain.Models;
+
+public class Message
+{
+    public Guid Id { get; set; }
+    public Guid TweetId { get; set; }
+    public string Content { get; set; }
+}
