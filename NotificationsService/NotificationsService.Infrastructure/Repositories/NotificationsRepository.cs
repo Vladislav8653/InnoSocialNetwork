63aaf289aaa56887364bfba1b351aed9aa028162
vlad <arefin.vlad@gmail.com>
2025-04-24 00:03:15 +0300

    #33 Feat: create presentation layer

---
+++ NotificationsService/NotificationsService.Infrastructure/Repositories/NotificationsRepository.cs
@@ -0,0 +1,40 @@
+﻿using NotificationsService.Application.Contracts.RepositoryContracts;
+using NotificationsService.Domain.Models;
+
+namespace NotificationsService.Infrastructure.Repositories;
+
+using MongoDB.Driver;
+
+public class NotificationRepository : INotificationRepository
+{
+    private readonly IMongoCollection<Notification> _collection;
+
+    public NotificationRepository(IMongoDatabase database)
+    {
+        _collection = database.GetCollection<Notification>("notifications");
+    }
+
+    public async Task<Notification?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var filter = Builders<Notification>.Filter.Eq(n => n.Id, id);
+        return await _collection.Find(filter).FirstOrDefaultAsync(cancellationToken);
+    }
+
+    public async Task CreateAsync(Notification notification, CancellationToken cancellationToken)
+    {
+        await _collection.InsertOneAsync(notification, null, cancellationToken);
+    }
+
+    public async Task DeleteAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var filter = Builders<Notification>.Filter.Eq(n => n.Id, id);
+        await _collection.DeleteOneAsync(filter, cancellationToken);
+    }
+
+    public async Task UpdateHangfireJobIdAsync(Guid id, string jobId, CancellationToken cancellationToken)
+    {
+        var filter = Builders<Notification>.Filter.Eq(n => n.Id, id);
+        var update = Builders<Notification>.Update.Set(n => n.HangfireJobId, jobId);
+        await _collection.UpdateOneAsync(filter, update, cancellationToken: cancellationToken);
+    }
+}
---
+++ NotificationsService/NotificationsService.Presentation/Controllers/NotificationController.cs
@@ -0,0 +1,84 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NotificationsService.Application.DataTransferObjects.NotificationsDto;
+using NotificationsService.Application.UseCases.Commands.NotificationCommands.CreateNotification;
+using NotificationsService.Application.UseCases.Commands.NotificationCommands.DeleteNotification;
+using NotificationsService.Application.UseCases.Commands.NotificationCommands.UpdateNotification;
+using NotificationsService.Application.UseCases.Queries.NotificationQueries.GetAllNotifications;
+using NotificationsService.Application.UseCases.Queries.NotificationQueries.GetNotificationById;
+
+namespace NotificationsService.Presentation.Controllers;
+
+[Route("api/notifications")]
+[ApiController]
+public class NotificationController(
+    IMediator mediator) : Controller
+{
+    [HttpGet("getNotifications")]
+    [Authorize(Policy = "User")]
+    public async Task<IActionResult> GetNotifications(CancellationToken cancellationToken)
+    {
+        var query = new GetAllNotificationsQuery();
+        var notifications = await mediator.Send(query, cancellationToken);
+        
+        return Ok(notifications);
+    }
+
+    [HttpGet("getNotificationById/{id}")]
+    [Authorize(Policy = "User")]
+    public async Task<IActionResult> GetNotificationById(Guid id, CancellationToken cancellationToken)
+    {
+        var query = new GetNotificationQuery
+        {
+            NotificationId = id
+        };
+        var notification = await mediator.Send(query, cancellationToken);
+        
+        return Ok(notification);
+    }
+    
+    [HttpPost("addNotification")]
+    [Authorize(Policy = "User")]
+    public async Task<IActionResult> AddNotification(
+        [FromBody] NotificationDto notificationDto,
+        CancellationToken cancellationToken)
+    {
+        var command = new CreateNotificationCommand
+        {
+            NotificationDto = notificationDto
+        };
+        
+        await mediator.Send(command, cancellationToken);
+        
+        return NoContent();
+    }
+
+    [HttpPut("updateNotification")]
+    [Authorize(Policy = "User")]
+    public async Task<IActionResult> UpdateNotification(
+        [FromBody] NotificationDto notificationDto,
+        CancellationToken cancellationToken)
+    {
+        var command = new UpdateNotificationCommand
+        {
+            NotificationDto = notificationDto
+        };
+        await mediator.Send(command, cancellationToken);
+        
+        return NoContent();
+    }
+
+    [HttpDelete("deleteNotification/{id}")]
+    [Authorize(Policy = "User")]
+    public async Task<IActionResult> DeleteNotification(Guid id, CancellationToken cancellationToken)
+    {
+        var command = new DeleteNotificationCommand
+        {
+            Id = id
+        };
+        await mediator.Send(command, cancellationToken);
+        
+        return NoContent();
+    }
+}
