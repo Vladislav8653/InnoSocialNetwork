1dec58a5469b85baee0d815d9db1cf1c3e3bd37a
vlad <arefin.vlad@gmail.com>
2025-04-23 15:24:06 +0300

    #33 Feat: create notification model

---
+++ NotificationsService/NotificationsService.Domain/Models/Notification.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace NotificationsService.Domain.Models;
+
+public class Notification
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.String)]
+    public Guid Id { get; set; } 
+    
+    public string EventType { get; set; } 
+    
+    public string Source { get; set; } 
+    
+    public DateTime CreatedAt { get; set; } 
+}
