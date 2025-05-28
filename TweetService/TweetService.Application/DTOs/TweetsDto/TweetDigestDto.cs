f433488b75b3415b9dd6bdaa60bb9c273b8f1c14
vlad <arefin.vlad@gmail.com>
2025-05-28 13:53:56 +0300

    #44 Feat: create handler for digest tweets request

---
+++ TweetService/TweetService.Application/DTOs/TweetsDto/TweetDigestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TweetService.Application.DTOs.TweetsDto;
+
+public record TweetDigestDto
+{
+    public string Title { get; init; } 
+    public string Content { get; init; } 
+    public DateTime Created { get; init; }
+    public DateTime Modified { get; init; } 
+}
