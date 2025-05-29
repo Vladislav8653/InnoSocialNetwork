0a687f488050ae894a47446856524fe7572355b0
vlad <arefin.vlad@gmail.com>
2025-05-29 12:26:59 +0300

    #44 Feat: create grpc service, which calls use case handler and transform data to grpc style

---
+++ TweetService/TweetService.Presentation/GrpcHandlers/TweetDigestService.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using MediatR;
+using TweetDigest.Grpc;
+using TweetService.Application.UseCases.Queries.Tweet.GetTweetsDigest;
+
+namespace TweetService.Presentation.GrpcHandlers;
+
+public class TweetDigestService(ISender sender) : TweetDigest.Grpc.TweetService.TweetServiceBase
+{
+    public override async Task<DigestResponse> GetDailyDigest(DigestRequest request, ServerCallContext context)
+    {
+        var query = new GetTweetsDigestCommand
+        {
+            StartDate =  DateTime.TryParse(request.From, out var startDate) ? startDate.ToUniversalTime() : default,
+            EndDate = DateTime.TryParse(request.To, out var endDate) ? endDate.ToUniversalTime() : default,
+        };
+        
+        var tweetsDigest = await sender.Send(query, context.CancellationToken);
+
+        var a = tweetsDigest.Select(item => new DigestItem
+        {
+            TweetId = item.Id.ToString(),
+            Content = item.Content,
+            CreatedAt = item.Created.ToString("o"),
+            ModifiedAt = item.Modified.ToString("o"),
+        });
+        
+        var response = new DigestResponse();
+        response.Items.Add(a);
+        return response;
+    }
+}
